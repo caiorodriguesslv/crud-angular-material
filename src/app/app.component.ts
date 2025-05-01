cd6e5b89342acee3767e7798f6621663056f2d2d
Caio Rodrigues <caio.rodrigues@indt.org.br>
2025-05-01 11:33:38 -0300

    Chore: ajuste no import do material pack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MATERIAL_IMPORTS } from './material.imports'; // importa os módulos centralizados
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MATERIAL_IMPORTS 
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'crud-angular-material';
---
+++ src/app/material.imports.ts
@@ -0,0 +1,33 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+export const MATERIAL_IMPORTS = [
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatTableModule,
+  MatSnackBarModule,
+  MatDialogModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatProgressSpinnerModule
+];
